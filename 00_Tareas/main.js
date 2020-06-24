5e1d9737d876fe487a7bfc9f8c4fce01d5362ba6
elnieves94 <nieves9427@gmail.com>
2020-06-23 19:18:03 -0500

    repaso

---
+++ 00_Tareas/main.js
@@ -0,0 +1,11 @@
+let numero = 550;
+
+let numeroBinario = numero.toString(2);
+console.log(numeroBinario);
+
+const decimalToBinary = (a) =>{
+    b = a.toString(2)
+    console.log(b);
+}
+
+decimalToBinary(550);
